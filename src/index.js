9e7c33c39503e9e6c933e602fc7f9907dd86d291
Paul Kögel <paul@railslove.com>
2016-08-11 23:21:15 +0200

    Enable HMR.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,20 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
+const rootEl = document.getElementById('root');
+
 ReactDOM.render(
   <App />,
-  document.getElementById('root')
+  rootEl
 );
+
+// enable hot module reloading (HMR) - cf. http://chrisshepherd.me/posts/adding-hot-module-reloading-to-create-react-app
+if (module.hot) {
+  module.hot.accept('./App', () => {
+    const NextApp = require('./App').default;
+    ReactDOM.render(
+      <NextApp />,
+      rootEl
+    );
+  });
+}
