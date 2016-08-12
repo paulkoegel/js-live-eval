5f205d0f426257b1b0ed77c24e8acb10ea9a410f
Paul Kögel <paul@railslove.com>
2016-08-12 14:34:02 +0200

    Cleanup.

--- src/playground/code.js
+++ src/playground/code.js
@@ -1,15 +1,17 @@
 import _ from 'lodash';
+
 let fn = () => {
 
-const squared = (n) => {
-  return n * n;
+  const squared = (n) => {
+    return n * n;
+  };
+
+  const values = [1, 2, 4];
+
+  return _.map(values, squared);
+
 };
 
-const values = [1, 2, 4];
-
-return _.map(values, squared);
-
-};
 export default {
   fnToEvaluate: fn,
   compiledSource: fn.toString()
